477cda113c52d6a2bc20705e691d3efa1ed11f76
valentineG <61587290+gatwirival@users.noreply.github.com>
2023-06-05 10:38:45 +0300

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,8 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  config.middleware.use Rack::MiniProfiler
+  Rack::MiniProfiler.config.position = 'bottom-left'
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
